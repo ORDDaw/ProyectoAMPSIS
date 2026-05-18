28ae35eee2e9398a72aac7af5beef37482d796a6
danielamdaw <githubdanielamdaw@gmail.com>
2026-05-18 11:21:09 +0200

    Prueab 1

--- src/themes/tema1/index.php
+++ src/themes/tema1/index.php
@@ -3,7 +3,7 @@
 <main class="hero">
   <section class="card">
     <span class="badge">Proyecto Ampliación</span>
-    <h1>Despliegue WordPress automatizado</h1>
+    <h1>Prueba 1</h1>
     <p>
       Esta pagina principal se actualiza desde GitHub mediante un contenedor Git,
       Docker Compose y Vagrant. Para publicar cambios solo hay que subirlos al
